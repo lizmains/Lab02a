a680eb402b296748554d41e64472811df432640d
Liz <emains@ycp.edu>
2022-02-18 01:40:53 -0500

    This is part one

---
+++ CS320_Lab02a_emains/src/edu/ycp/cs320/lab02a_emains/controller/GuessingGameController.java
@@ -0,0 +1,53 @@
+package edu.ycp.cs320.lab02a_emains.controller;
+
+import edu.ycp.cs320.lab02a_emains.model.GuessingGame;
+
+/**
+ * Controller for the guessing game.
+ */
+public class GuessingGameController {
+	private GuessingGame model;
+
+	/**
+	 * Set the model.
+	 * 
+	 * @param model the model to set
+	 */
+	public void setModel(GuessingGame model) {
+		this.model = model;
+	}
+
+	/**
+	 * Start a new guessing game by setting the minimum to 1 and the maximum to 100.
+	 */
+	public void startGame() {
+		model.setMin(1);
+		model.setMax(100);
+	}
+
+	/**
+	 * Called to indicate that the current guess is correct.
+	 * Set the min and max to the current guess.
+	 */
+	public void setNumberFound() {
+		model.setMin(model.getGuess());
+		model.setMax(model.getGuess());
+		model.isDone();
+	}
+
+	/**
+	 * Called to indicate that the user is thinking of a number that
+	 * is less than the current guess.
+	 */
+	public void setNumberIsLessThanGuess() {
+		model.setIsLessThan(model.getGuess());
+	}
+
+	/**
+	 * Called to indicate that the user is thinking of a number that
+	 * is greater than the current guess.
+	 */
+	public void setNumberIsGreaterThanGuess() {
+		model.setIsGreaterThan(model.getGuess());
+	}
+}
